f5a940bff3e7cf3a7f84c378573df39a328c5a79
Rachel Pierce <rachelpierce@students.berkeley.net>
2019-02-04 13:28:48 -0800

    added examples

--- src/Runner.java
+++ src/Runner.java
@@ -1,15 +1,30 @@
-public class CodingBat {
+public class Runner {
+    public static void main(String[] args) {
+        int[] arr={2,3,4,5,6};
+        int[] arr1={1,3,4};
+        System.out.println(makes10(5,2));
+        System.out.println(doubleX("xxhdnx"));
+        System.out.println(helloName("billybobjoe"));
+        System.out.println(no23(arr));
+        System.out.println(love6(3,5));
+        System.out.println(noTeenSum(3,4,5));
+        System.out.println(countHi("hibobjoehi"));
+        System.out.println(countTriple("sssndfgjdddgnsjkfg"));
+        System.out.println(lucky13(arr1));
+        System.out.println(linearIn(arr, arr1));
+    }
+
     //Warmup-1: makes10
-    public boolean makes10(int a, int b) {
-        if (a == 10 || b == 10 || (a + b) == 10) {
-            return true;
+    public static boolean makes10(int a, int b) {
+            if (a == 10 || b == 10 || (a + b) == 10) {
+                return true;
         } else {
             return false;
         }
     }
 
     //Warmup-2:doublex
-    boolean doubleX(String str) {
+    public static boolean doubleX(String str) {
         int i = str.indexOf("x");
         if (i == -1) {
             return false;
@@ -22,12 +37,12 @@ public class CodingBat {
     }
 
     //String-1:helloName
-    public String helloName(String name) {
+    public static String helloName(String name) {
         return ("Hello " + name + "!");
     }
 
     //Array-1: no23
-    public boolean no23(int[] nums) {
+    public static boolean no23(int[] nums) {
         if (nums[0] == 2 || nums[0] == 3 || nums[1] == 2 || nums[1] == 3) {
             return false;
         } else {
@@ -36,7 +51,7 @@ public class CodingBat {
     }
 
     //Logic-1: love6
-    public boolean love6(int a, int b) {
+    public static boolean love6(int a, int b) {
         if (a == 6 || b == 6 || (a + b) == 6 || (a - b) == 6 || (b - a) == 6) {
             return true;
         } else {
@@ -45,7 +60,7 @@ public class CodingBat {
     }
 
     //Logic-2: noTeenSum
-    public int noTeenSum(int a, int b, int c) {
+    public static int noTeenSum(int a, int b, int c) {
         if (a >= 13 && a <= 19 && a != 15 && a != 16) {
             a = 0;
         }
@@ -59,7 +74,7 @@ public class CodingBat {
     }
 
     //String-2: countHi
-    public int countHi(String str) {
+    public static int countHi(String str) {
         int num = 0;
         for (int i = 0; i < str.length() - 1; i++) {
             if (str.substring(i, i + 2).equals("hi")) {
@@ -70,7 +85,7 @@ public class CodingBat {
     }
 
     // String-3: countTriple
-    public int countTriple(String str) {
+    public static int countTriple(String str) {
         int num = 0;
         for (int i = 0; i < str.length() - 2; i++) {
             if (str.substring(i, i + 1).equals(str.substring(i + 1, i + 2)) && str.substring(i, i + 1).equals(str.substring(i + 2, i + 3))) {
@@ -81,7 +96,7 @@ public class CodingBat {
     }
 
     //Array-2: lucky 13
-    public boolean lucky13(int[] nums) {
+    public static boolean lucky13(int[] nums) {
         boolean a = true;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 1 || nums[i] == 3) {
@@ -92,7 +107,7 @@ public class CodingBat {
     }
 
     //Array-3: linerarIn
-    public boolean linearIn(int[] outer, int[] inner) {
+    public static boolean linearIn(int[] outer, int[] inner) {
         int num = 0;
         boolean b = false;
         for (int i = 0; i < inner.length; i++) {
